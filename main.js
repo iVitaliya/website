f022f621feb19328324a778c86254f4115c20d25
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-09-25 12:34:38 +0200

    Edited some stuff
    Added the css file, edited the .js file for server monitoring & edited the .html file.

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const express = require("express");
 
-/** @typedef {"HTTP" | "HTTPS"} Types */
-/**
- * @typedef IRepsonse
- * @property {Types} Type - The type of the response. (e.g.: http)
- * @property {number} Port - The port the server is running on
- * @property {string} [State] - The state of the server
- * @property {boolean} IsPaused - If the server is paused or running
- */
-/** @typedef {IReponse[]} IResponses */
+module.exports = () => {
+    /** @typedef {"HTTP" | "HTTPS"} Types */
+    /**
+     * @typedef IRepsonse
+     * @property {Types} Type - The type of the response. (e.g.: http)
+     * @property {number} Port - The port the server is running on
+     * @property {string} [State] - The state of the server
+     * @property {boolean} IsPaused - If the server is paused or running
+     */
+    /** @typedef {IReponse[]} IResponses */
 
-// Application
-const app = express.default();
+    // Application
+    const app = express();
 
-// HTTP/HTTPS Server
-const SERVER = http.createServer(app);
-SERVER.listen();
+    // HTTP/HTTPS Server
+    const SERVER = http.createServer(app);
+    SERVER.listen(80, null, null, () => {
+        console.log("Starting and serving server at port 80");
+    });
+    new http.Agent({ keepAlive: true });
+
+    const Connection = SERVER;
+    return Connection;
+};
