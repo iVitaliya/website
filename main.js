6f3b020954302cb502418519dc1279e75f6e53a0
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-09-25 10:08:26 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+
+/** @typedef {"HTTP" | "HTTPS"} Types */
+/**
+ * @typedef IRepsonse
+ * @property {Types} Type - The type of the response. (e.g.: http)
+ * @property {number} Port - The port the server is running on
+ * @property {string} [State] - The state of the server
+ * @property {boolean} IsPaused - If the server is paused or running
+ */
+/** @typedef {IReponse[]} IResponses */
+
+// Application
+const app = express.default();
+
+// HTTP/HTTPS Server
+const SERVER = http.createServer(app);
